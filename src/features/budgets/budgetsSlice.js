bfa852713e2f1c52d91cc49d2ddb7609eefa0180
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-31 00:03:53 +0000

    finished funds remaining and cleaned up

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -20,14 +20,15 @@ const budgetsSlice = createSlice({
   name: "budgets",
   initialState: initialState,
   reducers: {
+    // Set editBudget equal to a case reducer that receives two arguments—state and action
     editBudget: (state, action) => {
-      const newBudgets = state.map((budget) => {
-        if (budget.category === action.payload.category) {
-          return action.payload;
-        }
-        return budget;
-      });
-      return newBudgets;
+      // action.payload will have a category and amount property.
+      const category = action.payload.category;
+      const amount = action.payload.amount;
+      // Update the state by "finding" the budget object
+      // Note: the variables category and action, implemented below, are each assigned action.payload (referenced in the above const).
+      // The budget object whose .category value matches action.payload.category and changing with the .amount value to action.payload.amount.
+      state.find((budget) => budget.category === category).amount = amount;
     },
   },
 });
